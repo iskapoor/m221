9ac57c56087c09d088d1f8893867a73b41b59a91
Ishan Kapoor <falcon.kapoor@gmail.com>
2021-10-15 09:20:34 -0700

    created and implemented the leaderboard

--- src/main/java/com/example/sping_portfolio/controllers/Leaderboard.java
+++ src/main/java/com/example/sping_portfolio/controllers/Leaderboard.java
@@ -1,11 +1,25 @@
 package com.example.sping_portfolio.controllers;
+import com.example.sping_portfolio.unit7.GrayScale;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
+
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Leaderboard {
     @GetMapping("/leaderboard")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String page() {
-        return "Leaderboard"; // returns HTML VIEW (greeting)
+    public String page(Model model) {
+            String first = "Mort MORT MORT!";
+            String second = "Ishan";
+            String third = "Yeonjoon";
+
+
+            // returns the names and pictures to the HTML view
+            model.addAttribute("first", first);
+            model.addAttribute("second", second);
+            model.addAttribute("third", third);
+            return "leaderboard"; // returns HTML VIEW (greeting)
     }
 }
